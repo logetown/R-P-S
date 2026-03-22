e0ecc4d8c7c9b34ec2db0454baf83db609726670
Karthik <kodamkarthik33@gmail.com>
2026-03-22 17:16:39 +0530

    only rps files + images

--- fun.js
+++
@@ -1,7 +0,0 @@
-const buttonElement = document.querySelector('.js-button');
-const eventListener = () => { console.log('click1'); }
-
-buttonElement.addEventListener('click',eventListener);
-buttonElement.addEventListener('click',()=>{console.log('click2');});
-buttonElement.removeEventListener('click',eventListener);
-
